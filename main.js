736cd0810872beca070993a865d6c63469dc746b
stealthy-0 <futuristicmind2020@gmail.com>
2023-03-07 17:08:48 +0530

    modified js to make it more better

--- main.js
+++ main.js
@@ -20,37 +20,45 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
 
-  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.add("form--hidden");
-      createAccountForm.classList.remove("form--hidden");
+  document.querySelector("#linkCreateAccount").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
   });
 
-  document.querySelector("#linkLogin").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      createAccountForm.classList.add("form--hidden");
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
   });
 
-  loginForm.addEventListener("submit", e => {
-      e.preventDefault();
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-      // Perform your AJAX/Fetch login
+    // Perform your AJAX/Fetch login
 
-      setFormMessage(loginForm, "error", "Invalid username/password combination");
+    setFormMessage(loginForm, "error", "Invalid username/password combination."); // use this if backend returns failure to match credentials.
+    // setFormMessage(loginForm, "success", "Congratulations! you are logged in."); // use this if backend returns successful matching of credentials.
+
+    // Write code to redirect user to the dashboard of the website.
+  });
+  createAccountForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Perform your AJAX/Fetch login
+
+    setFormMessage(createAccountForm, "error", "Invalid username/password combination");
   });
 
-  document.querySelectorAll(".form__input").forEach(inputElement => {
-      inputElement.addEventListener("blur", e => {
-          if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-              setInputError(inputElement, "Username must be at least 10 characters in length");
-          }
-      });
+  document.querySelectorAll(".form__input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 6) {
+        setInputError(inputElement, "Username must be at least 6 characters in length");
+      }
+    });
 
-      inputElement.addEventListener("input", e => {
-          clearInputError(inputElement);
-      });
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
   });
 });
-
-
